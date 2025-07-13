df926d738d7209cabcbad11c0f61ee38be7a5151
kriscodesign <kriscodesign@gmail.com>
2025-07-13 22:47:55 +0100

    admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,6 +4,7 @@ from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
-    list_filter = ('title', 'author')
+    search_fields = ('title', 'author')
+    list_filter = ('status', 'due_back')
 
 admin.site.register(Book, BookAdmin)
