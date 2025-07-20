897abc13dc04dbf92434183eb6674ca3dd8c70ba
kriscodesign <kriscodesign@gmail.com>
2025-07-20 20:46:52 +0100

    task 2 login

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import RegisterationView
+from .views import register
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -8,9 +8,10 @@ urlpatterns = [
     path('<int:pk>/', views.LibraryDetailView.as_view(), 
          name='library_detail'),
 
-    path('register/', RegisterationView.as_view(), name='register'),
+    path('register/', views.register.as_view(), name='register'),
     path('login/', LoginView.as_view(
         template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', LogoutView.as_view(
+        template_name='relationship_app/logout.html'), name='logout'),
 
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -29,7 +29,7 @@ class LibraryDetailView(DetailView):
         context['books'] = library.books.all()
         return context
 
-class RegisterationView(CreateView):
+class register(CreateView):
     form_class = UserCreationForm
     template_name = 'relationship_app/register.html'
     success_url = reverse_lazy('login')
