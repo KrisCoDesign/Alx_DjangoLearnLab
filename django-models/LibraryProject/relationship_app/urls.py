e6da96036b5e73c3c251f3c230c977a9726fbc36
kriscodesign <kriscodesign@gmail.com>
2025-07-20 20:52:07 +0100

    task 2 login

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -9,9 +9,7 @@ urlpatterns = [
          name='library_detail'),
 
     path('register/', views.register.as_view(), name='register'),
-    path('login/', LoginView.as_view(
-        template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(
-        template_name='relationship_app/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
 ]
