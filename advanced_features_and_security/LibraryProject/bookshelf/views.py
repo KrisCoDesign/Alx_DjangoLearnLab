98d35624a1d24a36765c708b3f9a7ee1240b1e48
kriscodesign <kriscodesign@gmail.com>
2025-07-25 20:37:03 +0100

    task 1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from .models import CustomUser
-from django.contrib.auth.models import Group
+# from django.shortcuts import render
+# from .models import CustomUser
+# from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponse
 
-# Create your views here.
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def can_view(request):
+    return HttpResponse("Book viewing")
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def can_create(request):
+    return HttpResponse("Book Published")
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def can_edit(request):
+    return HttpResponse("Book edited")
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def can_delete(request):
+    return HttpResponse("Book deleted")
+
