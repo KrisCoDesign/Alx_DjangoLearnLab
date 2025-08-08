725a4fad66a4db0de5e104cb0afced10cbaccbc2
kriscodesign <kriscodesign@gmail.com>
2025-08-08 21:46:59 +0100

    task 3 test

--- advanced-api-project/api/tests.py
+++ advanced-api-project/api/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory, force_authenticate
+from .views import BookCreateView
+from .models import Book, Author
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class BookTest(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = get_user_model().objects.create_user('testuser', 'test@gmail.com', 'password')
+        self.author = Author.objects.create(name='Test Author')
+
+    def test_create_book(self):
+        data = {'title': 'Test Book ApiRequest', 
+                'publication_year': 2022, 
+                'author': self.author.id}
+        request = self.factory.post('/api/books/', data, format='json')
+        force_authenticate(request, user=self.user)
+        response = BookCreateView.as_view()(request)
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertEqual(Book.objects.get().title, 'Test Book ApiRequest')
