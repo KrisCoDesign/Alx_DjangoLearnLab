56ef979e47f15a022be869dea40c72af3a6b58a8
kriscodesign <kriscodesign@gmail.com>
2025-08-08 22:47:56 +0100

    task 3 test

--- advanced-api-project/api/tests_views.py
+++ advanced-api-project/api/tests_views.py
@@ -22,3 +22,24 @@ class BookTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Book.objects.count(), 1)
         self.assertEqual(Book.objects.get().title, 'Test Book ApiRequest')
+
+    def test_update_book(self):
+        self.author = Author.objects.create(name='Test Author')
+        self.book = Book.objects.create(title="1984", author=self.author, publication_year=1949)
+        url = reverse('book_update', args=[self.book.id])
+        data = {
+            'title': 'Test Book ApiRequest', 
+            'publication_year': 2022, 
+            'author': self.author.id}
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertEqual(Book.objects.get().title, 'Test Book ApiRequest')
+
+    def text_delete_book(self):
+        self.author = Author.objects.create(name='Test Author')
+        self.book = Book.objects.create(title="1984", author=self.author, publication_year=1949)
+        url = reverse('book_delete', args=[self.book.id])
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
