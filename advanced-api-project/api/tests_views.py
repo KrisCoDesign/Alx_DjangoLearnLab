2aea2f4ba89f07a369491fd9bed66490754f68f1
kriscodesign <kriscodesign@gmail.com>
2025-08-08 22:49:59 +0100

    task 3 test

--- advanced-api-project/api/tests_views.py
+++ advanced-api-project/api/tests_views.py
@@ -1,7 +1,6 @@
-from django.urls import reverse
 from rest_framework.test import APITestCase
+from django.urls import reverse
 from rest_framework import status
-# from .views import BookCreateView
 from .models import Book, Author
 from django.contrib.auth import get_user_model
 
