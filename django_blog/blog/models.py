2181cc08d472d76cb17da6e4593954673b90d4a8
kriscodesign <kriscodesign@gmail.com>
2025-08-11 14:57:06 +0100

    task 0

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    context = models.TextField()
+    content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
