c760b3b4b6a3677509f4362a5a7a2f4e3324c3d8
kriscodesign <kriscodesign@gmail.com>
2025-08-24 21:54:30 +0100

    task 04

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l24vnuqudp-w00j0(nhlnniivpb&qg7ec@xyb=^n9k3%b-sgr2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['krisco.pythonanywhere.com', 'localhost', '127.0.0.1']]
+ALLOWED_HOSTS = ['krisco.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
